1451c797339c23df8b99b7b7e72cd843b58da682
Harshini733 <harshinichaganti20@gmail.com>
2024-09-24 13:25:18 +0530

    getters and setters method added

--- src/myapp1/Employee.java
+++ src/myapp1/Employee.java
@@ -1,5 +1,27 @@
 package myapp1;
 
 public class Employee {
+	private int employeId;
+	private String employeeName;
+	private double employeeSalary;
+	public int getEmployeId() {
+		return employeId;
+	}
+	public void setEmployeId(int employeId) {
+		this.employeId = employeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	
 
 }
